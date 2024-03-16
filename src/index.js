f6d00b3770821f3543e863485dab425e6f08d081
Abhishek Gupta <bsaagupta77@gmail.com>
2024-03-16 16:43:44 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
-const { sendBasicEmail } = require('./services/email-service');
+const cron = require('node-cron');
+// const { sendBasicEmail } = require('./services/email-service');
 
 const setAndStartServer = () => {
   const app = express();
@@ -13,12 +14,17 @@ const setAndStartServer = () => {
 
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
-    sendBasicEmail(
-        'support@gmail.com',
-        'notificationservice.abhishek@gmail.com',
-        'This is a testing email',
-        'Hey! how are you , hope you like the support'
-    );
+    // sendBasicEmail(
+    //     'support@gmail.com',
+    //     'notificationservice.abhishek@gmail.com',
+    //     'This is a testing email',
+    //     'Hey! how are you , hope you like the support'
+    // );
+    
+    // cron.schedule('*/2 * * * *', ()=>{
+    //     console.log("running task every two minutes");
+    // })
+    
   })
 }
 
