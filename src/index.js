217d7e0387ae4e1c16a011066962756210031853
Abhishek Gupta <bsaagupta77@gmail.com>
2024-03-18 09:19:54 +0530

    added rabbit mq config

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const { createChannel } = require('./utils/messageQueue');
 
 const TicketController = require('./controllers/ticket-controller');
-const jobs = require('./utils/job');
+//const jobs = require('./utils/job');
 
-const setAndStartServer = () => {
+const setAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
+
+  // const channel = await createChannel();
 
   app.use('/api/v1/tickets',TicketController.create);
 
   app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
-    jobs();
+  //  jobs();
    
    
     
